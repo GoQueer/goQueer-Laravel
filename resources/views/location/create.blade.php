9a62359a1f0ee0cffce495a64ba2e171dddd9ae6
Bamdad Dehkordi <aghilide@ualberta.ca>
2016-11-21 22:05:42 -0700

    added required field to media

--- resources/views/location/create.blade.php
+++ resources/views/location/create.blade.php
@@ -42,7 +42,7 @@
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12" style="visibility: hidden">
             <div class="form-group">
-                {{ Form::select('user_id', '1') }}
+                {{ Form::hidden('user_id', '1', array('id' => 'user_id')) }}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
