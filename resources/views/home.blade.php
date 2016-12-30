deb9455968336da3671e29269b473a6874f77e71
bamzy <bamdad.ag@gmail.com>
2016-12-29 21:01:02 -0700

    changed some pictures on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -67,7 +67,7 @@
 
                 </div>
                 <header>
-                    <h3>Lorem ipsum<br /> dolor amet nullam</h3>
+                    <h3>How to Make a Map</h3>
                 </header>
                 <p>Morbi in sem quis dui placerat ornare. Pellentesquenisi<br />euismod in, pharetra a, ultricies in diam sed arcu. Cras<br />consequat  egestas augue vulputate.</p>
                 <footer>
@@ -79,7 +79,7 @@
                     <img src="{{ URL::to('/') }}/img/pic02.jpg"alt="Pic 02" />
                 </div>
                 <header>
-                    <h3>Sed feugiat<br /> tempus adipicsing</h3>
+                    <h3>How to Tell Your Story</h3>
                 </header>
                 <p>Pellentesque fermentum dolor. Aliquam quam lectus<br />facilisis auctor, ultrices ut, elementum vulputate, nunc<br /> blandit ellenste egestagus commodo.</p>
                 <footer>
